cfdaef4db15571b6a18f97beea21a885f22ad724
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-06-15 10:15:38 +0200

    improve read count accuracy
    In addition to returning the number of lines divided by 4, the number of lines only containing `+` has to match.

--- count-reads-fastq.py
+++ count-reads-fastq.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Credits go to:
+# With help from:
 #   https://codereview.stackexchange.com/questions/69261/counting-the-lines-of-a-compressed-file
 
 import argparse
@@ -7,6 +7,8 @@ import bz2
 import gzip
 import os
 import sys
+import re
+from progress.spinner import LineSpinner
 
 
 class UnsupportedCompressionException(Exception):
@@ -30,30 +32,31 @@ class CompressedFastqFile:
         self.fileCompressionTool = self.fileCompressionToolMap[extension]
 
     def count_reads(self):
-        line_count = self.count_lines()
-        if line_count % 4 == 0:
-            return line_count / 4
-        else:
-            raise UnexpectedLineCountException(
-                "Number of lines " + str(line_count) + " not a multiple of 4."
-            )
+        with LineSpinner('Reading file ') as spinner:
+            with self.fileCompressionTool(self.filepath) as fileHandler:
+                line_count = 0
+                lines_with_only_plus = 0
+                for line in fileHandler:
+                    spinner.next()
+                    line_count += 1
+                    if (re.match('\+', line.decode())):
+                        lines_with_only_plus += 1
 
-    def count_lines(self):
-        with self.fileCompressionTool(self.filepath) as fileHandler:
-            lines = 0
-            for _ in fileHandler:
-                lines += 1
-            return lines
-
+                if (line_count % 4 != 0):
+                    raise UnexpectedLineCountException( "Number of lines(" + str(line_count) + ") not a multiple of 4.")
+                elif (line_count / 4 != lines_with_only_plus):
+                    raise UnexpectedLineCountException( "Number of lines/4 (" + str(line_count / 4) +") and number of reads(" + str(lines_with_only_plus) + ") disagree.")
+                else:
+                    return lines_with_only_plus
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Count reads in fastq file.")
-    parser.add_argument("--file", nargs=1, help="a .bz2 or .gz compressed fastq file")
+    parser.add_argument("--file", nargs='+', help="a .bz2 or .gz compressed fastq file")
 
     args = parser.parse_args()
-    try:
-        compressed_file = CompressedFastqFile(args.file[0])
-        print(compressed_file.count_reads())
-    except (UnsupportedCompressionException, UnexpectedLineCountException) as e:
-        print(e)
-        exit(1)
+    for file_path in args.file:
+        try:
+                compressed_file = CompressedFastqFile(file_path)
+                print(file_path + ":\t" + str(compressed_file.count_reads()))
+        except (UnsupportedCompressionException, UnexpectedLineCountException) as e:
+            print(file_path + ":\t" + str(e), file=sys.stderr)
